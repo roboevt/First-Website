0ce0f4d154776da8504271cda444683db734b59b
roboevt <roboevt@gmail.com>
2021-10-19 10:15:41 -0500

    Added time since last click display

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 'use strict';
 const startBtn = document.querySelector('#start');
 const container = document.querySelector('#container');
-const target = document.querySelector('#target');
+const target = document.getElementById('target');
+const display = document.getElementById('display');
+
 let running = false;
 let width = container.clientWidth;
 let height = container.clientHeight;
+let lastClick = Date.now()
 
 startBtn.addEventListener('click', function() {
     if(running) running = false;
     else running = true;
+    console.log(running);
 });
 
 target.addEventListener('click', function() {
-    const target = document.getElementById("target");
     target.style.top = Math.random() * (width - target.clientHeight) + 'px';
     target.style.left = Math.random() * (height - target.clientWidth) + 'px';
+    display.innerHTML = Date.now() - lastClick + "ms";
+    lastClick = Date.now();
 });
