d28b0e107108a7559c8c3767bef98aed6d84f207
roboevt <roboevt@gmail.com>
2021-10-19 15:43:13 -0500

    Added average time

--- app.js
+++ app.js
@@ -7,17 +7,35 @@ const display = document.getElementById('display');
 let running = false;
 let width = container.clientWidth;
 let height = container.clientHeight;
-let lastClick = Date.now()
+let lastClick = Date.now();
+let firstClick = Date.now();
+let totalClicks = 0;
 
 startBtn.addEventListener('click', function() {
     if(running) running = false;
     else running = true;
-    console.log(running);
+    console.log("Running: " + running);
+    if(running) {
+        display.innerHTML = " ";
+        target.style.top = Math.random() * (width - target.clientHeight) + 'px';
+        target.style.left = Math.random() * (height - target.clientWidth) + 'px';
+        firstClick = Date.now();
+        totalClicks = 0;
+        startBtn.innerHTML = "Stop";
+    } else {
+        startBtn.innerHTML = "Start";
+        target.style.top = 0 + "px";
+        target.style.left = 0 + "px";
+    }
 });
 
 target.addEventListener('click', function() {
-    target.style.top = Math.random() * (width - target.clientHeight) + 'px';
-    target.style.left = Math.random() * (height - target.clientWidth) + 'px';
-    display.innerHTML = Date.now() - lastClick + "ms";
-    lastClick = Date.now();
+    if(running) {
+        target.style.top = Math.random() * (width - target.clientHeight) + 'px';
+        target.style.left = Math.random() * (height - target.clientWidth) + 'px';
+        display.innerHTML = "Last time: " + (Date.now() - lastClick) + "ms";
+        display.innerHTML += "<br /> Average Time: " + Math.round((Date.now() - firstClick) / totalClicks); + "ms";
+        lastClick = Date.now();
+        totalClicks++;
+    }
 });
